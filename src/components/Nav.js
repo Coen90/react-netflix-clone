7d59496b1b0102dbd75350b0b807015dbd08b5ce
hyuntae <bht9011@gmail.com>
2022-07-30 18:31:39 +0900

    study on weekend 20220730

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom'
 import "./Nav.css";
 
 function Nav() {
 
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -18,6 +21,11 @@ function Nav() {
       window.removeEventListener("scroll", () => {});
     }
   }, [])
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  }
   
 
   return (
@@ -26,8 +34,11 @@ function Nav() {
         alt='Netflix Logo'
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png'
         className='nav__logo'
-        onClick={() => window.location.reload()}
+        onClick={() => navigate(`/`)}
       />
+
+      <input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder='영화를 검색해주세요.' />
+
       <img
         alt='User logged'
         src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117'
--- src/components/Row.js
+++ src/components/Row.js
@@ -2,6 +2,16 @@ import axios from '../api/axios';
 import React, { useEffect, useState } from 'react'
 import './Row.css'
 import MovieModal from './MovieModal';
+// Import Swiper React components
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function Row({ isLargeRow, title, id, fetchUrl }) {
 
@@ -16,7 +26,6 @@ function Row({ isLargeRow, title, id, fetchUrl }) {
 
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
-    console.log("request", request);
     setMovies(request.data.results);
   }
   
@@ -28,37 +37,65 @@ function Row({ isLargeRow, title, id, fetchUrl }) {
   return (
     <section className='row'>
       <h2>{title}</h2>
-      <div className='slider'>
-        <div className='slider__arrow-left'>
-          <span className='arrow' 
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
+      {/* <div className='slider'>
+        <div className='slider__arrow-left'
+          onClick={() => {
+            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+          }}
+        >
+          <span className='arrow'>
             {"<"}
           </span>
-        </div>
+        </div> */}
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, 
+            slidesPerGroup: 6, 
+          },
+          998: {
+            slidesPerView: 5, 
+            slidesPerGroup: 5, 
+          },
+          625: {
+            slidesPerView: 4, 
+            slidesPerGroup: 4, 
+          },
+          0: {
+            slidesPerView: 3, 
+            slidesPerGroup: 3, 
+          },
+        }}
+      >
         <div id={id} className="row__posters">
           {movies.map(movie => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
+            <SwiperSlide >
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className='slider__arrow-right'>
-          <span className='arrow' 
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >
+        {/* <div className='slider__arrow-right'
+          onClick={() => {
+            document.getElementById(id).scrollLeft += window.innerWidth - 80;
+          }}
+        >
+          <span className='arrow' >
             {">"}
           </span>
         </div>
-      </div>
+      </div> */}
+      </Swiper>
 
             {
               modalOpen && (
